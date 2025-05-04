b0944004b5c496c721230050e7d9ca94215c6e9e
Roberth Ponce <rbrthpnc@gmail.com>
2025-05-03 18:51:19 -0600

    Nueva clase

---
+++ control.java
@@ -0,0 +1,10 @@
+public class control implements Runnable {
+    public void run() {
+        System.out.println("Hola mundo desde un hilo");
+    }
+    public static void main(String[] args) {
+        control hilo = new control();
+        Thread t = new Thread(hilo);
+        t.start();
+    
+}
